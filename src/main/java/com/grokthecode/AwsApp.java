571a0589620d8a8fabc9afb6bdd3573f89e5a268
Adrian De Leon <adrian@adriandeleon.me>
2022-05-23 15:32:00 -0500

    AwsApp.java: Added addEC2SecurityGroupRule() Added removeEC2SecurityGroupRule() Added getIpPermissionByDescription()

--- src/main/java/com/grokthecode/AwsApp.java
+++ src/main/java/com/grokthecode/AwsApp.java
@@ -9,50 +9,141 @@ import java.util.List;
 
 @Log4j2
 public class AwsApp {
+  private final Region region = Region.US_EAST_1;
 
   public void describeEc2Instances() {
     try {
-      final Region region = Region.US_EAST_1;
-      final Ec2Client ec2 = Ec2Client.builder().region(region).build();
+      final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
 
       final DescribeInstancesRequest request = DescribeInstancesRequest.builder().build();
-      final DescribeInstancesResponse response = ec2.describeInstances(request);
+      final DescribeInstancesResponse response = ec2Client.describeInstances(request);
 
       final List<Reservation> reservations = response.reservations();
+
       for (Reservation reservation : reservations) {
         final List<Instance> instances = reservation.instances();
 
         for (Instance instance : instances) {
-          System.out.println(instance.instanceId());
+          log.info(instance.instanceId());
         }
       }
-      ec2.close();
-      
+      ec2Client.close();
+
     } catch (Ec2Exception e) {
-      System.err.println(e.awsErrorDetails().errorMessage());
+      log.error(e.awsErrorDetails().errorMessage());
       System.exit(1);
     }
   }
 
-  public static void describeEC2SecurityGroups(final String groupId) {
+  public void describeEC2SecurityGroups(final String groupId) {
+    final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
     try {
-      final Region region = Region.US_EAST_1;
-      final Ec2Client ec2 = Ec2Client.builder().region(region).build();
-
       final DescribeSecurityGroupsRequest request =
           DescribeSecurityGroupsRequest.builder().groupIds(groupId).build();
 
-      final DescribeSecurityGroupsResponse response = ec2.describeSecurityGroups(request);
+      final DescribeSecurityGroupsResponse response = ec2Client.describeSecurityGroups(request);
 
       for (SecurityGroup group : response.securityGroups()) {
-        System.out.printf(
-            "Found Security Group with id %s, " + "vpc id %s " + "and description %s",
-            group.groupId(), group.vpcId(), group.description());
+        log.info(String.format("Found Security Group with id %s, " + "vpc id %s " + "and description %s",
+            group.groupId(), group.vpcId(), group.description()));
       }
-      ec2.close();
+      ec2Client.close();
     } catch (Ec2Exception e) {
-      System.err.println(e.awsErrorDetails().errorMessage());
+      log.error(e.awsErrorDetails().errorMessage());
       System.exit(1);
     }
+  }
+
+  public void addEC2SecurityGroupRule(final String groupId, final String ruleDescription,
+                                      final String protocol, final String ip) {
+    final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
+
+    try {
+      String authorizedIp = ip + "/32";
+
+      final IpRange ipRange = IpRange.builder()
+              .description(ruleDescription)
+              .cidrIp(authorizedIp)
+              .build();
+
+      final IpPermission ipPermission = IpPermission.builder()
+              .ipProtocol(protocol)
+              .ipRanges(ipRange)
+              .build();
+
+      final AuthorizeSecurityGroupIngressRequest authorizeSecurityGroupIngressRequest = AuthorizeSecurityGroupIngressRequest.builder()
+              .groupId(groupId)
+              .ipPermissions(ipPermission)
+              .build();
+
+      final AuthorizeSecurityGroupIngressResponse authorizeSecurityGroupIngressResponse = ec2Client.authorizeSecurityGroupIngress(authorizeSecurityGroupIngressRequest);
+
+      ec2Client.close();
+    } catch (Ec2Exception e) {
+      log.error(e.awsErrorDetails().errorMessage());
+      System.exit(1);
+    }
+  }
+
+  public void removeEC2SecurityGroupRule(final String groupId, final String ruleDescription) {
+    try {
+      final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
+      final IpPermission ipPermission = getIpPermissonByDescription(groupId, ruleDescription);
+
+      final RevokeSecurityGroupIngressRequest revokeSecurityGroupIngressRequest = RevokeSecurityGroupIngressRequest.builder()
+              .groupId(groupId)
+              .ipPermissions(ipPermission)
+              .build();
+
+      final RevokeSecurityGroupIngressResponse revokeSecurityGroupIngressResponse = ec2Client.revokeSecurityGroupIngress(revokeSecurityGroupIngressRequest);
+
+      ec2Client.close();
+    } catch (Ec2Exception e) {
+      log.error(e.awsErrorDetails().errorMessage());
+      System.exit(1);
+    }
+  }
+
+  public IpPermission getIpPermissonByDescription(final String groupId, final String ruleDescription) {
+    final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
+    IpPermission ipPermissionFound = null;
+    try {
+      final DescribeSecurityGroupsRequest request = DescribeSecurityGroupsRequest.builder()
+              .groupIds(groupId)
+              .build();
+
+      final DescribeSecurityGroupsResponse response = ec2Client.describeSecurityGroups(request);
+      String cidrIp = null;
+      String description = null;
+      String protocol = null;
+
+      for (SecurityGroup group : response.securityGroups()) {
+        for (IpPermission ipPermission : group.ipPermissions()) {
+          for (IpRange ipRange : ipPermission.ipRanges()) {
+            if (ipRange.description().equals(ruleDescription)) {
+              log.info(ipRange.description());
+              cidrIp = ipRange.cidrIp();
+              description = ipRange.description();
+              protocol = ipPermission.ipProtocol();
+            }
+          }
+        }
+      }
+      final IpRange iprange = IpRange.builder()
+              .cidrIp(cidrIp)
+              .description(description)
+              .build();
+      ipPermissionFound = IpPermission.builder()
+              .ipRanges(iprange)
+              .ipProtocol(protocol)
+              .build();
+
+      ec2Client.close();
+
+    } catch (Ec2Exception e) {
+      log.error(e.awsErrorDetails().errorMessage());
+      System.exit(1);
+    }
+    return ipPermissionFound;
   }
 }
--- src/test/java/com/grokthecode/AwsAppTest.java
+++ src/test/java/com/grokthecode/AwsAppTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 class AwsAppTest {
 
   private static AwsApp awsApp;
+  private final String ruleDescription = "GitLab Backup";
+  private final String groupId = "sg-03e1f3f58783316c8";
 
   @BeforeAll
   static void setup() {
@@ -13,12 +15,30 @@ class AwsAppTest {
   }
 
     @Test
-    void describeEc2Instances() {
+    void describeEc2InstancesTest() {
       awsApp.describeEc2Instances();
     }
 
   @Test
-  void describeSecurityGroups() {
-    awsApp.describeEC2SecurityGroups("sg-03e1f3f58783316c8");
+  void describeSecurityGroupsTest() {
+    awsApp.describeEC2SecurityGroups(groupId);
+  }
+
+  @Test
+  void addEC2SecurityGroupsTest() {
+    final HttpClientApp httpClientApp = new HttpClientApp();
+    final String ip = httpClientApp.getIPFromHttpBinToDto().getOrigin();
+
+    awsApp.addEC2SecurityGroupRule(groupId, ruleDescription, "-1", ip);
+  }
+
+  @Test
+  void removeEC2SecurityGroupRuleTest() {
+    awsApp.removeEC2SecurityGroupRule(groupId, ruleDescription);
+  }
+
+  @Test
+  void getIpPermissonByDescriptionTest() {
+    awsApp.getIpPermissonByDescription(groupId, ruleDescription);
   }
 }
