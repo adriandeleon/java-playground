4e1d05c4377dd14bc724b8e75323cbef51dc55f6
Adrian De Leon <adrian@adriandeleon.me>
2022-05-27 12:59:59 -0500

    AwsApp.java: Added getInstancesByTagNameValue(), modified the getInstance to be by id instead of name.

--- src/main/java/com/grokthecode/AwsApp.java
+++ src/main/java/com/grokthecode/AwsApp.java
@@ -1,22 +1,75 @@
 package com.grokthecode;
 
 import lombok.extern.log4j.Log4j2;
-import software.amazon.awssdk.awscore.exception.AwsServiceException;
-import software.amazon.awssdk.core.exception.SdkClientException;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ec2.Ec2Client;
 import software.amazon.awssdk.services.ec2.model.*;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Log4j2
 public class AwsApp {
-  private final Region region = Region.US_EAST_1;
+  private final Region awsRegion = Region.US_EAST_1;
+
+  public List<String> getInstancesByTagNameValue(final String tagNameValue) {
+    final Ec2Client ec2Client = Ec2Client.builder().region(awsRegion).build();
+
+    final var request = DescribeInstancesRequest.builder().build();
+    final var response = ec2Client.describeInstances(request);
+
+    final List<String> instanceIdList = new ArrayList<>();
+
+    for (Reservation reservation : response.reservations()) {
+      for (Instance instance : reservation.instances()) {
+        for (Tag tag : instance.tags()) {
+          if (tag.key().equals("Name")) {
+            if (tag.value().contains(tagNameValue)) {
+              instanceIdList.add(instance.instanceId());
+            }
+          }
+        }
+      }
+    }
+    return instanceIdList;
+  }
+
+  public Optional<Instance> getInstanceByInstanceId(final String instanceId){
+    try {
+      final Ec2Client ec2Client =  Ec2Client.builder().region(awsRegion).build();
+
+      final DescribeInstancesRequest request = DescribeInstancesRequest.builder().build();
+      final DescribeInstancesResponse response = ec2Client.describeInstances(request);
+
+      for (Reservation reservation : response.reservations()) {
+        for (Instance instance : reservation.instances()) {
+          if(instance.instanceId().equals(instanceId)) {
+            return Optional.of(instance);
+          }
+        }
+      }
+      ec2Client.close();
+
+    } catch (Ec2Exception e) {
+      log.error(e.awsErrorDetails().errorMessage());
+      System.exit(1);
+    }
+    return Optional.empty();
+  }
+
+  public List<String> getSecurityGroupByInstanceId(final String instanceId){
+
+  final Optional<Instance> optionalInstance = getInstanceByInstanceId(instanceId);
+  final List<String> securityGroupId = new ArrayList<>();
+
+     for (GroupIdentifier groupIdentifier : optionalInstance.get().securityGroups() ) {
+       securityGroupId.add(groupIdentifier.groupId());
+     }
+     return  securityGroupId;
+  }
 
   public void describeEc2Instances() {
     try {
-      final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
+      final Ec2Client ec2Client =  Ec2Client.builder().region(awsRegion).build();
 
       final DescribeInstancesRequest request = DescribeInstancesRequest.builder().build();
       final DescribeInstancesResponse response = ec2Client.describeInstances(request);
@@ -39,7 +92,7 @@ public class AwsApp {
   }
 
   public void describeEC2SecurityGroups(final String groupId) {
-    final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
+    final Ec2Client ec2Client =  Ec2Client.builder().region(awsRegion).build();
     try {
       final DescribeSecurityGroupsRequest request =
           DescribeSecurityGroupsRequest.builder().groupIds(groupId).build();
@@ -59,7 +112,7 @@ public class AwsApp {
 
   public void addEC2SecurityGroupRule(final String groupId, final String ruleDescription,
                                       final String protocol, final String ip) {
-    final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
+    final Ec2Client ec2Client =  Ec2Client.builder().region(awsRegion).build();
 
     try {
       String authorizedIp = ip + "/32";
@@ -90,7 +143,7 @@ public class AwsApp {
 
   public void removeEC2SecurityGroupRule(final String groupId, final String ruleDescription) throws Exception {
     try {
-      final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
+      final Ec2Client ec2Client =  Ec2Client.builder().region(awsRegion).build();
       final Optional<IpPermission> optionalIpPermission = getIpPermissonByDescription(groupId, ruleDescription);
       final IpPermission ipPermission = optionalIpPermission.get();
 
@@ -109,7 +162,7 @@ public class AwsApp {
   }
 
   public Optional<IpPermission> getIpPermissonByDescription(final String groupId, final String ruleDescription) throws Exception {
-    final Ec2Client ec2Client =  Ec2Client.builder().region(region).build();
+    final Ec2Client ec2Client =  Ec2Client.builder().region(awsRegion).build();
     IpPermission ipPermissionFound = null;
     try {
       final DescribeSecurityGroupsRequest request = DescribeSecurityGroupsRequest.builder()
@@ -157,6 +210,7 @@ public class AwsApp {
   public void updateEC2SecurityGroupRule(final String groupId, final String ruleDescription, final String ip) {
     try{
       final Optional<IpPermission> optionalIpPermission = getIpPermissonByDescription(groupId, ruleDescription);
+      //final String protocol = optionalIpPermission.get().ipProtocol();
       final String protocol = optionalIpPermission.get().ipProtocol();
 
       updateEC2SecurityGroupRule(groupId, ruleDescription, protocol, ip);
--- src/test/java/com/grokthecode/AwsAppTest.java
+++ src/test/java/com/grokthecode/AwsAppTest.java
@@ -2,16 +2,40 @@ package com.grokthecode;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.ec2.model.Instance;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class AwsAppTest {
 
   private static AwsApp awsApp;
   private final String ruleDescription = "GitLab Backup";
-  private final String groupId = "sg-03e1f3f58783316c8";
+  private final String securityGroupId = "sg-03e1f3f58783316c8";
 
   @BeforeAll
   static void setup() {
     awsApp = new AwsApp();
+  }
+
+  @Test
+  void getInstancesByTagNameValueTest() {
+    final List<String> instancesList = awsApp.getInstancesByTagNameValue("servidor-win");
+    assertThat(instancesList).isNotNull();
+  }
+
+  @Test
+  void getInstanceFromIdTest() {
+    Optional<Instance> optionalInstance = awsApp.getInstanceByInstanceId("i-0912954d5bf659903");
+    assertThat(optionalInstance.get()).isNotNull();
+  }
+
+  @Test
+  void getSecurityGroupFromInstanceIdTest() {
+    final List<String> securityGroupIdList = awsApp.getSecurityGroupByInstanceId("servidor-win");
+    assertThat(securityGroupIdList).isNotNull();
   }
 
     @Test
@@ -21,24 +45,34 @@ class AwsAppTest {
 
   @Test
   void describeSecurityGroupsTest() {
-    awsApp.describeEC2SecurityGroups(groupId);
+    awsApp.describeEC2SecurityGroups(securityGroupId);
   }
 
   @Test
-  void addEC2SecurityGroupsTest() {
-    final HttpClientApp httpClientApp = new HttpClientApp();
-    final String ip = httpClientApp.getIPFromHttpBinToDto().getOrigin();
-
-    awsApp.addEC2SecurityGroupRule(groupId, ruleDescription, "-1", ip);
+  void addEC2SecurityGroupRuleTest() {
+    awsApp.addEC2SecurityGroupRule(securityGroupId, ruleDescription, "-1", getPublicIp());
   }
 
   @Test
   void removeEC2SecurityGroupRuleTest() throws Exception {
-    awsApp.removeEC2SecurityGroupRule(groupId, ruleDescription);
+    awsApp.removeEC2SecurityGroupRule(securityGroupId, ruleDescription);
   }
 
   @Test
   void getIpPermissonByDescriptionTest() throws Exception {
-    awsApp.getIpPermissonByDescription(groupId, ruleDescription);
+    awsApp.getIpPermissonByDescription(securityGroupId, ruleDescription);
+  }
+
+  @Test
+  void updateEC2SecurityGroupRuleTest(){
+    final HttpClientApp httpClientApp = new HttpClientApp();
+    final String ip = httpClientApp.getIPFromHttpBinToDto().getOrigin();
+
+    awsApp.updateEC2SecurityGroupRule(securityGroupId, ruleDescription, getPublicIp());
+  }
+
+  private String getPublicIp(){
+    final HttpClientApp httpClientApp = new HttpClientApp();
+    return httpClientApp.getIPFromHttpBinToDto().getOrigin();
   }
 }
